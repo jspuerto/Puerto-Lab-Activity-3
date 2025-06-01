2f82010ebe402822ef0cb185bf819bb2ab1c0df0
Juliana <jspuerto@up.edu.ph>
2025-06-02 02:18:30 +0800

    remove fallback route for serving index.html in serveStatic function

--- server/vite.ts
+++ server/vite.ts
@@ -77,7 +77,4 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
-  });
 }
